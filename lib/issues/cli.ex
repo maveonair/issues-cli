c2bc9456fd66228f99f6c6b2c54a5d20b3f7a81d
Fabian Mettler <mettler.fabian@gmail.com>
2015-04-06 13:26:30 +0200

    Fix a typo

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,7 +2,7 @@ defmodule Issues.CLI do
   @default_count 4
   @moduledoc """
   Handle the command line parsing and the dispatch to the various functions that
-  end up generating a table of the last _n_n issues in a github project
+  end up generating a table of the last _n_ issues in a github project
   """
 
   def main(argv) do
